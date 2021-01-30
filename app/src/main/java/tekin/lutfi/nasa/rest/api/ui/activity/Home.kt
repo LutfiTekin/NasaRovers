a39d5baa0614970a35dccff576c1164ed45730ec
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-01-30 16:36:33 +0300

    refactor: Home activity file moved to activity folder

--- app/src/main/java/tekin/lutfi/nasa/rest/api/ui/activity/Home.kt
+++ app/src/main/java/tekin/lutfi/nasa/rest/api/ui/activity/Home.kt
@@ -1,4 +1,4 @@
-package tekin.lutfi.nasa.rest.api
+package tekin.lutfi.nasa.rest.api.ui.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import tekin.lutfi.nasa.rest.api.R
 
 class Home : AppCompatActivity() {
 
