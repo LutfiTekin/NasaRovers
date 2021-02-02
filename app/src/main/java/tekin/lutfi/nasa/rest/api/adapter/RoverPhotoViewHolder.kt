ac9dce453b46b6f03245dbff55fa6dc450c51279
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-02-02 21:59:29 +0300

    handle image load error

--- app/src/main/java/tekin/lutfi/nasa/rest/api/adapter/RoverPhotoViewHolder.kt
+++ app/src/main/java/tekin/lutfi/nasa/rest/api/adapter/RoverPhotoViewHolder.kt
@@ -1,15 +1,18 @@
 package tekin.lutfi.nasa.rest.api.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.request.ImageRequest
+import coil.request.ImageResult
 import tekin.lutfi.nasa.rest.api.R
 import tekin.lutfi.nasa.rest.api.model.Photo
-import tekin.lutfi.nasa.rest.api.toConsole
 
 class RoverPhotoViewHolder internal constructor(
     inflater: LayoutInflater,
@@ -18,12 +21,42 @@ class RoverPhotoViewHolder internal constructor(
 ) : RecyclerView.ViewHolder(inflater.inflate(res, parent, false)){
 
     private val imageView = itemView.findViewById<AppCompatImageView>(R.id.imageView)
+    private val camName = itemView.findViewById<AppCompatTextView>(R.id.camName)
+    private var loadedPhoto: Photo? = null
+
+    private val retryClickListener = View.OnClickListener {
+        bind(loadedPhoto)
+    }
+
+    init {
+        camName.setOnClickListener(retryClickListener)
+    }
 
     fun bind(photo: Photo?){
+        camName.isClickable = false
+        camName.text = photo?.camera?.fullName.orEmpty()
+        camName.isVisible = false
+        loadedPhoto = photo
         imageView.load(photo?.imgSrc){
             crossfade(true)
             placeholder(R.drawable.ic_baseline_satellite_24)
+            this.listener(object : ImageRequest.Listener{
+                override fun onError(request: ImageRequest, throwable: Throwable) {
+                    super.onError(request, throwable)
+                    imageView.setImageResource(R.drawable.ic_baseline_error_outline_24)
+                    camName.text = itemView.context.getString(R.string.image_load_error)
+                    camName.isVisible = true
+                    camName.isClickable = true
+                }
+                override fun onSuccess(request: ImageRequest, metadata: ImageResult.Metadata) {
+                    super.onSuccess(request, metadata)
+                    camName.isVisible = true
+                }
+            })
         }
     }
+
+
+
 
 }
